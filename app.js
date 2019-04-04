cd8cfe102c3f5ecd8b59b924ad6b016a82fa5031
Benjamin Zhang <bzhang7@outlook.com>
2019-04-03 22:04:04 -0400

    1. Add data

---
+++ app.js
@@ -0,0 +1,35 @@
+const db = {
+  'vacation ideas': [
+    {
+      title: '10 Budget-Friendly Family Summer Vacation Ideas | Travel Channel',
+      link: 'https://www.travelchannel.com/interests/family/photos/budget-friendly-family-summer-vacation-ideas',
+      description: '10 Budget-Friendly Family Summer Vacation Ideas. Texas Hill Country River Region. It may get hot in Texas during the summer, but that makes a relaxing float along the crystal-clear Frio River in the Texas Hill Country River Region that much better. Minneapolis. Florida\'s Nature Coast. Albuquerque. Niagara Falls. ...'
+    },
+    {
+      title: 'The 50 Best Places to Travel in 2019 | Travel + Leisure',
+      link: 'https://www.travelandleisure.com/trip-ideas/best-places-to-travel-in-2019',
+      description: 'Need vacation ideas for 2019? The 50 vacation destinations Travel + Leisure has ranked as the best include new capitals of culture, idyllic remote.'
+    },
+    {
+      title: 'Trip Ideas: Vacations, Tours & Getaways | Travel + Leisure',
+      link: 'https://www.travelandleisure.com/trip-ideas',
+      description: 'Need a trip idea? Whether you\'re a beach bum, adventure junkie, or devout worshipper of the weekend getaway: yes, there\'s a trip for that.'
+    },
+    {
+      title: '25 Bucket List Vacation Ideas for the New Year',
+      link: 'https://www.travelpulse.com/gallery/destinations/25-bucket-list-vacation-ideas-for-the-new-year.html',
+      description: 'If you\'re tired of visiting the same rental condo, theme park, or resort year after year, right now is the perfect time to change up your plans.'
+    },
+    {
+      title: 'Affordable U.S. Destinations for 2019',
+      link: 'https://www.aarp.org/travel/travel-tips/budget/info-2019/inexpensive-us-trips.html',
+      description: 'Instead of visiting well-known cheap vacation spots in the U.S., venture out and try one of our affordable vacation destinations.'
+    },
+    {
+      title: 'Vacation Ideas & Popular Destinations',
+      link: 'https://www.tripadvisor.com/Inspiration',
+      description: 'Looking for some inspiration for your next trip? Find great vacation ideas and inspiration from TripAdvisor, your source for the web\'s best reviews and travel ...'
+    }
+  ],
+};
+
