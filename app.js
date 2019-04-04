68b6a799fe4b897432bea55643545eb097cf6982
Benjamin Zhang <bzhang7@outlook.com>
2019-04-03 23:41:44 -0400

    3. Finalized

--- app.js
+++ app.js
@@ -31,26 +31,96 @@ const db = {
       description: 'Looking for some inspiration for your next trip? Find great vacation ideas and inspiration from TripAdvisor, your source for the web\'s best reviews and travel ...'
     }
   ],
+  'what to watch on netflix': [
+    {
+      title: '100 Best Movies on Netflix Right Now (Updated April 2019) - Vulture',
+      link: 'https://www.vulture.com/article/best-movies-on-netflix-right-now.html',
+      description: 'The best movies on Netflix include Pulp Fiction, The Departed, Silence ... choice easy to find, it can be difficult to know what to watch on Netflix. ',
+    },
+    {
+      title: 'New on Netflix April 2019: Movies, TV and Original Series - Vulture',
+      link: 'https://www.vulture.com/article/new-on-netflix-movies-shows-originals.html',
+      description: 'Across the Line. All the President\'s Men. Chilling Adventures of Sabrina: Part 2 — Netflix Original. New Girl: Season 7. Black Summer — Netflix Original. A Land Imagined — Netflix Film. A Fortunate Man — Netflix Film. I Think You Should Leave With Tim Robinson — Netflix Original. The Hateful Eight: Extended Version.',
+    },
+    {
+      title: 'The best TV shows to watch on Netflix - Radio Times',
+      link: 'https://www.radiotimes.com/news/on-demand/2019-04-03/best-tv-shows-netflix/',
+      description: 'Looking for your next binge watch? You\'ve come to the right place. Advertisement. Netflix is adding new original series and old favourites all ...',
+    },
+    {
+      title: 'Best Netflix Series and Movies to Binge-Watch Now - Rotten Tomatoes',
+      link: 'https://editorial.rottentomatoes.com/guide/best-netflix-shows-and-movies-to-binge-watch-now/',
+      description: 'Netflix changed the way we rent movies and watch TV shows when it launched in 1997, then innovated again when it introduced its Netflix Originals in 2011 with ...',
+    },
+    {
+      title: 'The Best Shows on Netflix April 2019 - A Guide to What to Watch Now',
+      link: 'https://www.tomsguide.com/us/pictures-story/870-best-tv-shows-netflix.html',
+      description: 'Admit it: binge-watching is now a way of life. Here are the best TV shows on Netflix, with seasons that you\'ll finish in a single weekend.',
+    },
+    {
+      title: 'The 50 Best Movies on Netflix Right Now - The New York Times',
+      link: 'https://www.nytimes.com/interactive/2019/arts/television/best-movies-on-netflix.html',
+      description: 'We\'ve plucked out the 50 best films currently streaming on Netflix in the United States. Take a look.',
+    },
+  ],
+  'how to get accepted by hogwarts': [
+    {
+      title: 'How to get into Hogwarts - Quora',
+      link: 'https://www.quora.com/How-do-you-get-into-Hogwarts',
+      description: 'Originally Answered: How do I get accepted into Hogwarts School of Witchcraft and Wizardry? ... So, if you are a *muggle, or a squib** you cannot attend Hogwarts School of Witchcraft and Wizardry, and any other magical schools. ... Usually students get their letter by age of eleven for ...'
+    },
+    {
+      title: 'The Quill of Acceptance and The Book of Admittance - Pottermore',
+      link: 'https://www.pottermore.com/writing-by-jk-rowling/the-quill-of-acceptance-and-the-book-of-admittance',
+      description: 'These are the Quill of Acceptance and the Book of Admittance and they constitute the only process by which students are selected for Hogwarts School of ...'
+    },
+    {
+      title: '10 Signs You\'re Not Going to Receive Your Hogwarts Acceptance Letter',
+      link: 'http://www.collegehumor.com/post/7054168/10-signs-youre-not-going-to-receive-your-hogwarts-acceptance-letter',
+      description: 'And that\'s why the idea of a Hogwarts acceptance letter struck such a deep chord in so many - people have an easy time relating to Harry Potter ...'
+    },
+    {
+      title: 'You Can Now Attend Hogwarts School Of Witchcraft And Wizardry ...',
+      link: 'https://www.huffingtonpost.co.uk/2014/04/16/you-can-now-attend-hogwarts-school-online_n_5159514.html',
+      description: 'Have you always dreamt of receiving a letter telling you you are in fact a witch or wizard and have been admitted to Hogwarts School of ...'
+    },
+    {
+      title: 'Hogwarts acceptance letter | Harry Potter Wiki | FANDOM powered by ...',
+      link: 'https://harrypotter.fandom.com/wiki/Hogwarts_acceptance_letter',
+      description: 'But we\'ll get the letter, you and me." — Discussing of Hogwarts school. The Hogwarts letter is a letter of acceptance to Hogwarts School of Witchcraft and ...'
+    },
+    {
+      title: 'You\'re Only Allowed To Go To Hogwarts If You Can Get 11/15 On This Quiz',
+      link: 'https://www.buzzfeed.com/karlaagis/harry-potter',
+      description: 'What are the names of the Hogwarts founders? Pottermore. Godric Gryffindor, Helga Hufflepuff, Rhea Ravenclaw, and Salazar Slytherin.'
+    }
+  ]
 };
 
 function searchWithKeyWords(keyWords) {
-  const results = db[keyWords];
+  const results = db[keyWords] || [];
   const resultsPage = document.querySelector('#results-page');
   const resultHtmlParts = [];
 
   results.forEach(res => {
-    let resHtml = `<li><a class="result-link" href="${res.link}">${res.title}</a><div>${res.description}</div></li>`;
+    let resHtml = `<li class="list-item"><a class="result-link" href="${res.link}">${res.title}</a><div>${res.description}</div></li>`;
     resultHtmlParts.push(resHtml);
   });
 
-  resultsPage.innerHTML = `<ul>${resultHtmlParts.join('\n')}</ul>`;
+  resultsPage.innerHTML = `<ul id="results-list" class="uk-list">${resultHtmlParts.join('\n')}</ul>`;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const searchBtn = document.querySelector('#search-button');
   const searchInput = document.querySelector('#search-input');
 
-  searchBtn.addEventListener('click', () => searchWithKeyWords(searchInput.value));
+  searchInput.focus();
+
+  searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    searchWithKeyWords(searchInput.value);
+  });
 
   searchInput.addEventListener('keyup', (ev) => {
     if (ev.keyCode === 13) {
